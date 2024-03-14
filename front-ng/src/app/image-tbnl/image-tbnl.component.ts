8e4f0975811dc89829e87358e466ade40ff2fe3e
calebxzhou <portableryanc69@outlook.com>
2024-03-15 00:15:17 +0800

    1

--- front-ng/src/app/image-tbnl/image-tbnl.component.ts
+++ front-ng/src/app/image-tbnl/image-tbnl.component.ts
@@ -1,37 +1,43 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { HttpEvent, HttpEventType } from '@angular/common/http';
-import { Subscription } from 'rxjs';
-import { MediaService } from '../media/media.service';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { HttpEvent, HttpEventType } from "@angular/common/http";
+import { Subscription } from "rxjs";
+import { MediaService } from "../media/media.service";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from "@angular/animations";
 @Component({
-  selector: 'lg-image-tbnl',
+  selector: "lg-image-tbnl",
   standalone: true,
   imports: [CommonModule, MatProgressSpinnerModule],
-  templateUrl: './image-tbnl.component.html',
+  templateUrl: "./image-tbnl.component.html",
   styles: `
   `,
-    animations: [
-      trigger('scaleAnimation', [
-        state('initial', style({ transform: 'scale(0.01)' })),
-        state('normal', style({ transform: 'scale(1)' })),
-        transition('initial => normal', animate('500ms ease-in'))
-      ])
-    ]
+  animations: [
+    trigger("scaleAnimation", [
+      state("initial", style({ transform: "scale(0.01)" })),
+      state("normal", style({ transform: "scale(1)" })),
+      transition("initial => normal", animate("500ms ease-in")),
+    ]),
+  ],
 })
 // 缩略图
-export class ImageTbnlComponent implements OnInit   {
+export class ImageTbnlComponent implements OnInit {
   @Input() url!: string;
-  @Input() isVideo: boolean = false; 
-  state: string = 'initial';
-  videoDuration = "视频"; 
+  @Input() isVideo: boolean = false;
+  state: string = "initial";
+  videoDuration = "视频";
 
   constructor(private ms: MediaService) {}
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
     setTimeout(() => {
-      this.state = 'normal';
+      this.state = "normal";
     }, 0);
     if (this.isVideo) {
       this.fetchDuration();
@@ -39,8 +45,7 @@ export class ImageTbnlComponent implements OnInit   {
   }
 
   async fetchDuration() {
-    let sec = await this.ms.getVideoDuration(this.url.replaceAll("?tbnl=1",""));
-    this.videoDuration = this.ms.formatDuration(sec);
+    //let sec = await this.ms.getVideoDuration(this.url.replaceAll("?tbnl=1",""));
+    this.videoDuration = "视频"; //this.ms.formatDuration(sec);
   }
- 
 }
