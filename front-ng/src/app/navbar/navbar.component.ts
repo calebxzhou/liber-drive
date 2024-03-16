89a7e3d521b6d20e408e77c0b894a2d88adc4cd7
calebxzhou <portableryanc69@outlook.com>
2024-03-16 22:09:14 +0800

    nav auto hide

--- front-ng/src/app/navbar/navbar.component.ts
+++ front-ng/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, HostListener, Input } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -19,14 +19,31 @@ import { Router, ActivatedRoute } from "@angular/router";
 .spacer {
   flex: 1 1 auto;
 }
-
+.hide-toolbar {
+      transform: translateY(-100%);
+      transition: transform 0.3s ease-in-out;
+    }
   `,
 })
 export class NavbarComponent {
-  @Input() title!:string;
+  @Input() title!: string;
   constructor(private router: Router, private route: ActivatedRoute) {}
   back() {
     // Use the navigate method with a relative path
     this.router.navigate(["../"], { relativeTo: this.route });
+  }
+  hideToolbar = false;
+  lastScrollTop = 0;
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll() {
+    let currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScrollTop > this.lastScrollTop) {
+      this.hideToolbar = true;
+    } else {
+      this.hideToolbar = false;
+    }
+    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
   }
 }
