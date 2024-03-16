c18981a9bcefee1ee0f0ca161e3a8b4cc92ef18d
calebxzhou <portableryanc69@outlook.com>
2024-03-16 11:30:01 +0800

    2.3

--- front-ng/src/app/home/home.component.ts
+++ front-ng/src/app/home/home.component.ts
@@ -1,22 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { AlbumPreviewComponent } from '../album-preview/album-preview.component';
-import { Title } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { AlbumPreviewComponent } from "../album-preview/album-preview.component";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'lg-home',
+  selector: "lg-home",
   standalone: true,
-  imports: [CommonModule, AlbumPreviewComponent,RouterModule],
-  templateUrl: './home.component.html',
-  styles: ``
+  imports: [CommonModule, AlbumPreviewComponent, RouterModule],
+  templateUrl: "./home.component.html",
+  styles: ``,
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router,private titleService: Title) {}
+  constructor(private router: Router, private titleService: Title) {}
   ngOnInit(): void {
-    this.titleService.setTitle("嘉乐周的光影世界 2.1")
+    this.titleService.setTitle("嘉乐周的光影世界 2.3");
   }
-  goGallery(name:string){
-    this.router.navigate(['/gallery/'+name])
+  goGallery(name: string) {
+    this.router.navigate(["/gallery/" + name]);
   }
 }
